009bdade2c07663c04bd066aa0937d13d1310390
m.nivet89 <m.nivet89@3e7b8514-7694-6713-e55f-4d5dc9e558ac>
2011-10-24 13:17:52 +0000

    - Correction du temps en double - Ajout du SimulateurTaxis.java

--- trunk/src/simulateur/Client.java
+++ trunk/src/simulateur/Client.java
@@ -5,9 +5,9 @@ import java.awt.geom.Point2D;
 public class Client {
 
 	private Point2D.Double depart, arrivee;
-	private int tempsAttente;
+	private double tempsAttente;
 
-	public Client(Point2D.Double depart, Point2D.Double arrivee, int tempsAttente) {
+	public Client(Point2D.Double depart, Point2D.Double arrivee, double tempsAttente) {
 		this.depart = depart;
 		this.arrivee = arrivee;
 		this.tempsAttente = tempsAttente;
