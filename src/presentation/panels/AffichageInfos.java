291d52bf1cc9571c3c6fba6395cf989721f8fabe
remi.lacroix <remi.lacroix@3e7b8514-7694-6713-e55f-4d5dc9e558ac>
2011-11-05 15:44:59 +0000

    Cosmétiques : utilisation du formatage par défaut d'Eclipse modifié pour avoir un retour à la ligne après 120 caractères (Ctrl + Maj + F) pour uniformiser le style de tous les fichiers.
    Ajout des identifiants de sérialisations pour faire plaisir à Eclipse.

--- src/presentation/panels/AffichageInfos.java
+++ src/presentation/panels/AffichageInfos.java
@@ -12,60 +12,62 @@ import javax.swing.JPanel;
 /**
  * Un panneau d'affichage du temps qui passe
  */
-public class AffichageInfos extends JPanel
-{
+public class AffichageInfos extends JPanel {
+
+	private static final long serialVersionUID = -4708213254353783988L;
+
 	/** Le temps actuel */
 	private int temps;
 	/** Le nombre de taxis */
 	private int nbTaxis;
-	
+
 	/**
-	 *  Construit un affichage du temps
+	 * Construit un affichage du temps
 	 */
-	public AffichageInfos()
-	{
+	public AffichageInfos() {
 		super();
 		this.setPreferredSize(new Dimension(0, 30));
 	}
-	
+
 	/**
 	 * Dessine le panneau avec le temps actuel
 	 */
-	public void paintComponent(Graphics g)
-	{
+	public void paintComponent(Graphics g) {
 		Graphics2D g2D = (Graphics2D) g;
-		
+
 		// Pour améliorer le rendu
 		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2D.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-		
+
 		// Le fond noir de l'affichage
 		g2D.setColor(Color.black);
 		g2D.fillRect(0, 0, this.getWidth(), this.getHeight());
-		
+
 		// Affichage du texte représentant le temps et du nombre de taxis
 		Font font = new Font("Arial", Font.BOLD, 20);
 		g2D.setFont(font);
-		g2D.setColor(Color.white);          
+		g2D.setColor(Color.white);
 		g2D.drawString("Temps actuel : " + temps, 10, 20);
 		g2D.drawString("Nombre de taxis : " + nbTaxis, this.getWidth() - 220, 20);
 	}
-	
+
 	/**
 	 * Modifie le temps actuel
-	 * @param tempsActuel le temps actuel
+	 * 
+	 * @param tempsActuel
+	 *            le temps actuel
 	 */
-	public void setTemps(int tempsActuel)
-	{
+	public void setTemps(int tempsActuel) {
 		temps = tempsActuel;
 	}
 
 	/**
 	 * Modifie le nombre de taxis
-	 * @param nbTaxis le nombre de taxis
+	 * 
+	 * @param nbTaxis
+	 *            le nombre de taxis
 	 */
-	public void setNbTaxis(int nbTaxis)
-	{
+	public void setNbTaxis(int nbTaxis) {
 		this.nbTaxis = nbTaxis;
 	}
 }
--- src/presentation/panels/AffichageInit.java
+++ src/presentation/panels/AffichageInit.java
@@ -11,102 +11,109 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 
 /**
- * Des onglets pour les différentes simulations avec pour chacun un formulaire pour rentrer les paramètres de la simulation
+ * Des onglets pour les différentes simulations avec pour chacun un formulaire
+ * pour rentrer les paramètres de la simulation
  */
-public class AffichageInit extends JTabbedPane
-{	
+public class AffichageInit extends JTabbedPane {
+
+	private static final long serialVersionUID = 2757677226841283740L;
+
 	/** L'onglet pour la configuration de la recherche du nombre de taxis */
 	private JPanel tabNbTaxis;
 	/** L'onglet pour la configuration du calcul du pourcentage */
 	private JPanel tabNbClients;
-	/** Les champs des paramètres avec leurs valeurs pour la configuration de la recherche du nombre de taxis */
-	private HashMap<String, JTextField> champsValeurs1;
-	/** Les champs des paramètres avec leurs valeurs pour la configuration du calcul du pourcentage */
-	private HashMap<String, JTextField> champsValeurs2;
-	
 	/**
-	 *  Construit un affichage pour rentrer les paramètres sous forme de deux onglets.
-	 *  Le premier pour paramétrer la recherche du nombre de taxis à avoir pour prendre en charge un certain pourcentage de clients.
-	 *  Le second pour paramétrer le calcul du pourcentage de clients pris en charge par un certain nombre de taxis.
+	 * Les champs des paramètres avec leurs valeurs pour la configuration de la
+	 * recherche du nombre de taxis
 	 */
-	public AffichageInit(String[] labels1, int[] widths1, String[] labels2, int[] widths2)
-	{
+	private HashMap<String, JTextField> champsValeurs1;
+	/**
+	 * Les champs des paramètres avec leurs valeurs pour la configuration du
+	 * calcul du pourcentage
+	 */
+	private HashMap<String, JTextField> champsValeurs2;
+
+	/**
+	 * Construit un affichage pour rentrer les paramètres sous forme de deux
+	 * onglets. Le premier pour paramétrer la recherche du nombre de taxis à
+	 * avoir pour prendre en charge un certain pourcentage de clients. Le second
+	 * pour paramétrer le calcul du pourcentage de clients pris en charge par un
+	 * certain nombre de taxis.
+	 */
+	public AffichageInit(String[] labels1, int[] widths1, String[] labels2, int[] widths2) {
 		super();
-		
-	    tabNbTaxis = new JPanel(new BorderLayout());
-	    tabNbClients = new JPanel(new BorderLayout());
-	    
-	    champsValeurs1 = new HashMap<String, JTextField>();
-	    champsValeurs2 = new HashMap<String, JTextField>();
 
-	    // Crée le premier formulaire pour la recherche du nombre de taxis
-	    JPanel labelPanel1 = new JPanel(new GridLayout(labels1.length, 1));
-	    JPanel fieldPanel1 = new JPanel(new GridLayout(labels1.length, 1));
-	    tabNbTaxis.add(labelPanel1, BorderLayout.WEST);
-	    tabNbTaxis.add(fieldPanel1, BorderLayout.CENTER);
-	    JTextField[] fields1 = new JTextField[labels1.length];
+		tabNbTaxis = new JPanel(new BorderLayout());
+		tabNbClients = new JPanel(new BorderLayout());
 
-	    for (int i = 0; i < labels1.length; i += 1)
-	    {
-	      fields1[i] = new JTextField();
-	      if (i < widths1.length)
-	        fields1[i].setColumns(widths1[i]);
+		champsValeurs1 = new HashMap<String, JTextField>();
+		champsValeurs2 = new HashMap<String, JTextField>();
 
-	      JLabel lab = new JLabel(labels1[i], JLabel.RIGHT);
-	      lab.setLabelFor(fields1[i]);
+		// Crée le premier formulaire pour la recherche du nombre de taxis
+		JPanel labelPanel1 = new JPanel(new GridLayout(labels1.length, 1));
+		JPanel fieldPanel1 = new JPanel(new GridLayout(labels1.length, 1));
+		tabNbTaxis.add(labelPanel1, BorderLayout.WEST);
+		tabNbTaxis.add(fieldPanel1, BorderLayout.CENTER);
+		JTextField[] fields1 = new JTextField[labels1.length];
 
-	      labelPanel1.add(lab);
-	      JPanel p = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	      p.add(fields1[i]);
-	      fieldPanel1.add(p);
-	      
-	      champsValeurs1.put(labels1[i], fields1[i]);
-	    }
-	    
-	    // Crée le second formulaire pour le calcul du pourcentage
-	    JPanel labelPanel2 = new JPanel(new GridLayout(labels2.length, 1));
-	    JPanel fieldPanel2 = new JPanel(new GridLayout(labels2.length, 1));
-	    tabNbClients.add(labelPanel2, BorderLayout.WEST);
-	    tabNbClients.add(fieldPanel2, BorderLayout.CENTER);
-	    JTextField[] fields2 = new JTextField[labels2.length];
+		for (int i = 0; i < labels1.length; i += 1) {
+			fields1[i] = new JTextField();
+			if (i < widths1.length)
+				fields1[i].setColumns(widths1[i]);
 
-	    for (int i = 0; i < labels2.length; i += 1)
-	    {
-	      fields2[i] = new JTextField();
-	      if (i < widths2.length)
-	        fields2[i].setColumns(widths2[i]);
+			JLabel lab = new JLabel(labels1[i], JLabel.RIGHT);
+			lab.setLabelFor(fields1[i]);
 
-	      JLabel lab = new JLabel(labels2[i], JLabel.RIGHT);
-	      lab.setLabelFor(fields2[i]);
+			labelPanel1.add(lab);
+			JPanel p = new JPanel(new FlowLayout(FlowLayout.LEFT));
+			p.add(fields1[i]);
+			fieldPanel1.add(p);
 
-	      labelPanel2.add(lab);
-	      JPanel p = new JPanel(new FlowLayout(FlowLayout.LEFT));
-	      p.add(fields2[i]);
-	      fieldPanel2.add(p);
-	      
-	      champsValeurs2.put(labels2[i], fields2[i]);
-	    }
-		
+			champsValeurs1.put(labels1[i], fields1[i]);
+		}
+
+		// Crée le second formulaire pour le calcul du pourcentage
+		JPanel labelPanel2 = new JPanel(new GridLayout(labels2.length, 1));
+		JPanel fieldPanel2 = new JPanel(new GridLayout(labels2.length, 1));
+		tabNbClients.add(labelPanel2, BorderLayout.WEST);
+		tabNbClients.add(fieldPanel2, BorderLayout.CENTER);
+		JTextField[] fields2 = new JTextField[labels2.length];
+
+		for (int i = 0; i < labels2.length; i += 1) {
+			fields2[i] = new JTextField();
+			if (i < widths2.length)
+				fields2[i].setColumns(widths2[i]);
+
+			JLabel lab = new JLabel(labels2[i], JLabel.RIGHT);
+			lab.setLabelFor(fields2[i]);
+
+			labelPanel2.add(lab);
+			JPanel p = new JPanel(new FlowLayout(FlowLayout.LEFT));
+			p.add(fields2[i]);
+			fieldPanel2.add(p);
+
+			champsValeurs2.put(labels2[i], fields2[i]);
+		}
+
 		this.addTab("Chercher le nombre de taxis", tabNbTaxis);
 		this.addTab("Calculer le pourcentage de clients pris en charge", tabNbClients);
 	}
-	
+
 	/**
 	 * Retourne une map qui à chaque nom de paramètre associe sa valeur
+	 * 
 	 * @return une map qui à chaque nom de paramètre associe sa valeur
 	 */
-	public HashMap<String, String> getValues()
-	{
+	public HashMap<String, String> getValues() {
 		HashMap<String, String> result = new HashMap<String, String>();
 		HashMap<String, JTextField> values = null;
-		
-		if(this.getSelectedIndex() == 0)
+
+		if (this.getSelectedIndex() == 0)
 			values = champsValeurs1;
 		else
 			values = champsValeurs2;
-		
-		for(String label : values.keySet())
-		{
+
+		for (String label : values.keySet()) {
 			result.put(label, values.get(label).getText());
 		}
 		return result;
