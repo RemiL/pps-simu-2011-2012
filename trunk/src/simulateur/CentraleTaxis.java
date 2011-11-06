9d19665483794582b1b5f540c6797457d51b4799
remi.lacroix <remi.lacroix@3e7b8514-7694-6713-e55f-4d5dc9e558ac>
2011-11-06 12:45:30 +0000

    Ajout de Javadoc pour les classes CentraleTaxis et ReferentielTemps.

--- trunk/src/simulateur/CentraleTaxis.java
+++ trunk/src/simulateur/CentraleTaxis.java
@@ -4,17 +4,55 @@ import java.awt.geom.Point2D;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
+/**
+ * Classe représentant une centrale de taxis.
+ * 
+ * Une centrale de taxis gère un certain nombre fixé de taxis qu'elle dispatche
+ * en fonction des appels des clients. Elle maintient une liste d'attente pour
+ * les clients qui n'ont pas encore pu être affectés à un taxi et un certain
+ * nombre d'informations statistiques notammment le nombre de clients qui ont
+ * été perdus parce que pas servis assez rapidement.
+ */
 public class CentraleTaxis {
-
+	/**
+	 * Le référentiel de temps partagé par tous les parties prenantes du
+	 * problème (centrale des taxis, taxis et clients).
+	 */
 	private ReferentielTemps referentielTemps;
 
+	/** Tableau des taxis rattaché à la centrale */
 	private Taxi[] taxis;
+	/** La liste des clients ayant demandé un taxi */
 	private LinkedList<Client> clientsEnAttente;
+	/**
+	 * La position de la centrale de taxi dans le repère gradué en mètres ayant
+	 * pour origine le centre de la ville.
+	 */
 	private Point2D.Double position;
+	/** Le nombre de clients ayant appelé la centrale depuis son ouverture */
 	private int nbClients;
+	/** Le nombre de clients perdus depuis l'ouverture de la centrale */
 	private int nbClientsPerdus;
 
-	public CentraleTaxis(ReferentielTemps referentielTemps, int nombreTaxis, Point2D.Double position, double vitesse, int nbClientsMax) {
+	/**
+	 * Construit une centrale possédant les caractéristiques fournies et dont
+	 * l'état initial correspond à celui à l'ouverture.
+	 * 
+	 * @param referentielTemps
+	 *            le référentiel de temps commun à toutes les parties prenantes
+	 * @param nombreTaxis
+	 *            le nombre de taxis rattachés à la centrale
+	 * @param position
+	 *            la position de la centrale dans le repère gradué en mètres
+	 *            ayant pour origine le centre de la ville
+	 * @param vitesse
+	 *            la vitesse des taxis
+	 * @param nbClientsMax
+	 *            le nombre maximal de clients pouvant être transportés par un
+	 *            taxi
+	 */
+	public CentraleTaxis(ReferentielTemps referentielTemps, int nombreTaxis, Point2D.Double position, double vitesse,
+			int nbClientsMax) {
 		this.referentielTemps = referentielTemps;
 
 		this.position = position;
@@ -29,6 +67,12 @@ public class CentraleTaxis {
 		this.nbClientsPerdus = 0;
 	}
 
+	/**
+	 * Essaie d'affecter les taxis aux clients en attente en fonction de leur
+	 * disponibilité et de leur proximité avec le client : pour chaque client le
+	 * taxi disponible (s'il existe) le plus proche du point de départ du client
+	 * est envoyé.
+	 */
 	public void affecterTaxis() {
 		ListIterator<Client> it = clientsEnAttente.listIterator();
 		Client client;
@@ -45,7 +89,7 @@ public class CentraleTaxis {
 			// d'optimiser le processus au niveau informatique sans modifier
 			// pour autant le résultat de la simulation.
 			if (client.estEnAttenteTaxi()) {
-				// On envoi le taxi disponible le plus proche du client.
+				// On envoie le taxi disponible le plus proche du client.
 				distanceMin = Double.MAX_VALUE;
 				meilleurTaxi = null;
 
@@ -74,33 +118,74 @@ public class CentraleTaxis {
 		}
 	}
 
+	/**
+	 * Fait rouler les taxis rattachés à la centrale vers leur destination
+	 * actuelle (s'ils en ont une) pendant une durée dt.
+	 */
 	public void deplacerTaxis() {
 		for (Taxi taxi : taxis) {
 			taxi.rouler();
 		}
 	}
 
+	/**
+	 * Ajoute un nouveau client à la liste des clients en attente d'un taxi.
+	 * 
+	 * @param client
+	 *            le nouveau client désirant prendre un taxi
+	 */
 	public void ajouterClient(Client client) {
 		clientsEnAttente.add(client);
 		nbClients++;
 	}
 
+	/**
+	 * Incrémente d'une unité le compteur de clients perdus.
+	 */
 	public void signalerClientPerdu() {
 		nbClientsPerdus++;
 	}
 
+	/**
+	 * Retourne le nombre de clients ayant fait appel aux services de la
+	 * centrale de taxi.
+	 * 
+	 * @return le nombre de clients ayant fait appel aux services de la centrale
+	 *         de taxi
+	 */
 	public int getNbClients() {
 		return nbClients;
 	}
 
+	/**
+	 * Retourne le nombre de clients perdus par la centrale de taxis parce
+	 * qu'ils n'ont pas été pris en charge par un taxi suffisemment rapidement à
+	 * leur goût.
+	 * 
+	 * @return le nombre de clients perdus par la centrale de taxis parce qu'ils
+	 *         n'ont pas été pris en charge par un taxi suffisemment rapidement
+	 *         à leur goût
+	 */
 	public int getNbClientsPerdus() {
 		return nbClientsPerdus;
 	}
 
+	/**
+	 * Retourne les taxis rattachés à la centrale de taxis.
+	 * 
+	 * @return le tableau des taxis rattachés à la centrale
+	 */
 	public Taxi[] getTaxis() {
 		return taxis;
 	}
 
+	/**
+	 * Retourne la liste des clients non pris en charge par un taxi (mais
+	 * potentiellement déjà affectés à un taxi).
+	 * 
+	 * @return la liste des clients non pris en charge par un taxi (mais
+	 *         potentiellement déjà affectés à un taxi)
+	 */
 	public LinkedList<Client> getClientsNonPrisEnCharge() {
 		return clientsEnAttente;
 	}
